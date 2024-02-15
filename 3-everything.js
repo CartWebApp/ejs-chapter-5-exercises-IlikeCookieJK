811f467b2f08a87c060c3792d8767aeb66fcfa57
Jun <jun_hong.kuang23@student.cart.org>
2024-02-15 15:48:22 -0800

    everything2

--- 3-everything.js
+++ 3-everything.js
@@ -6,10 +6,20 @@ function every(array, test) {
     }
     return true;
   }
-  
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+
+function every(array, test) {
+  const akagi = (array) => test(array) === false;
+  if (array.some(akagi) === false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
