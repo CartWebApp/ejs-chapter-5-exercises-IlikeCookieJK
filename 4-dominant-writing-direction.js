16f0eece8fbb209bb17460b9aecd4445987cde0a
IlikeCookieJK <jun_hong.kuang23@student.cart.org>
2024-02-15 20:38:14 -0800

    direction
    line 16

--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,10 +1,36 @@
-require('./includes/scripts.js') // include the scripts used by the chapter
+const SCRIPTS = require('./includes/scripts.js');
 
-function dominantDirection(text) {
-    // Your code here.
+require('./includes/scripts.js') // include the scripts used by the chapter
+// let c = console.log;
+// function dominantDirection(text) {
+//   for (x of SCRIPTS) {
+//     c(groupName(x))
+//   }
+// }
+
+function cS(x) {
+  for (let z of SCRIPTS) {
+    if (z.ranges.some(([from, to]) => {
+      return x >= from && x < to;
+    })) {
+      console.log(z.direction)
+      return z;
+    }
   }
+  return null;
+}
+console.log(cS(121));
+
+
+// function CountBy (x, gn) {
+//   let z = []
+//   for (let xx of x) {
+//     let name = gn(xx);
+//     let known
+//   }
+// }
   
-  console.log(dominantDirection("Hello!"));
+  // console.log(dominantDirection("Hello!"));
   // → ltr
-  console.log(dominantDirection("Hey, مساء الخير"));
+  // console.log(dominantDirection("Hey, مساء الخير"));
   // → rtl
